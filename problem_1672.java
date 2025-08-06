13924fd9640ae679ece2502afb719e4137fdc80c
Manas <official.manastiwari2101@gmail.com>
2025-08-06 23:10:57 +0530

    Create problem_1672.java
    easy-medium

---
+++ problem_1672.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int max=0;
+        for(int i=0;i<accounts.length;i++){
+            int sum=0;
+            for(int j=0;j<accounts[i].length;j++){
+                sum=sum+accounts[i][j];
+            }
+            if(sum>max){
+                max=sum;
+            }
+        }
+
+        return max;
+    }
+}
